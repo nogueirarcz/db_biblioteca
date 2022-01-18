95c4245e337326de25719e08e2a1fc616d1b8362
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2022-01-18 08:46:41 -0300

    Aula 07
    Consultando dados com consultas simples

---
+++ scripts/aula-07.sql
@@ -0,0 +1,11 @@
+# Consultando dados
+# Consulta simples
+
+# Consulta apenas os nomes dos autores na tabela de autores
+select nome_autor from tbl_autor;
+
+# Consulta todas as informações dos autores
+select * from tbl_autor;
+
+# Consulta o nome do livro e o id do autor
+select nome_livro, id_autor from tbl_livro;
